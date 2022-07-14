568ff49ce6fd65e78894c6874471d72423c2a841
Vitor Fernandes Moraes <80361373+Vitor-php@users.noreply.github.com>
2022-07-13 23:58:15 -0300

    feat: create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const server =  express();
+
+server.get('/', (req,res) => {
+    return res.json({mensagem: 'API está funcionando!'})
+});
+
+server.listen(3000, () => {
+    console.log('Servidor está funcionando')
+});
