447c063f46456ca5a9f15efaefcf4bd03cff350e
Vitor Fernandes Moraes <80361373+Vitor-php@users.noreply.github.com>
2022-07-14 00:18:00 -0300

    feat: install nodemon

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const server =  express();
 
 server.get('/', (req,res) => {
-    return res.json({mensagem: 'API está funcionando!'})
-});
+    return res.json({mensagem: 'API está funcionando' })
+})
 
 server.listen(3000, () => {
     console.log('Servidor está funcionando')
